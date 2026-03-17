a958d665f01e9206df2b10bc9f235fb5957063a3
Rizz 2425 <mohdrizwanahmed1996@gmail.com>
2026-03-17 14:50:47 +0530

    Added validation

--- predictor/views.py
+++ predictor/views.py
@@ -8,21 +8,21 @@ with open('student_model.pkl', 'rb') as f:
 def home(request):
     result = None
     if request.method == 'POST':
-        
         study_hours = float(request.POST.get('study_hours'))
         attendance = float(request.POST.get('attendance'))
         prev_score = float(request.POST.get('prev_score'))
 
-        
-        input_data = pd.DataFrame([[study_hours, attendance, prev_score]], 
-                                 columns=['Study_Hours', 'Attendance', 'Previous_Score'])
-
-        
-        prediction = model.predict(input_data)
-        
-        if prediction[0] == 1:
-            result = "Pass (Congratulations!)"
+        # Backend Check
+        if 0 <= study_hours <= 24 and 0 <= attendance <= 100 and 0 <= prev_score <= 100:
+            input_data = pd.DataFrame([[study_hours, attendance, prev_score]], 
+                                     columns=['Study_Hours', 'Attendance', 'Previous_Score'])
+            prediction = model.predict(input_data)
+            
+            if prediction[0] == 1:
+                result = "Pass (Congratulations!)"
+            else:
+                result = "Fail (Needs Improvement)"
         else:
-            result = "Fail (Needs Improvement)"
+            result = "Error: Please enter valid range values!"
 
     return render(request, 'index.html', {'result': result})
